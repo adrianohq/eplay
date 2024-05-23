6d6f26e8eef055720acfd9a36fb7a0ff573333f6
Adriano Henrique <adrianohqueiroz95@gmail.com>
2024-05-22 21:33:16 -0300

    Componetizando os produtos

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,17 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Infos, Titulo } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum assumenda
-      sunt, perferendis dicta enim, veritatis quisquam veniam iure ipsam et
-      libero odio soluta dolore quae quaerat! Facilis recusandae illo porro!
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
